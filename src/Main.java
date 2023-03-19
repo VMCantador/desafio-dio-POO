a5c35cee3c28c9f9858f22b53ee6a2c51459800c
Vezio <vezio.cantador@etec.sp.gov.br>
2023-03-19 19:25:33 -0300

    Finalizando desafio

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
-import java.sql.Date;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
+import java.time.LocalDate;
 
 public class Main {
 
@@ -13,22 +12,57 @@ public class Main {
 		Curso curso1 = new Curso();
 		curso1.setTitulo("Curso Java");
 		curso1.setDescricao("Orientação a objeto no Java");
-		curso1.setCargaHoraria(2);
+		curso1.setCargaHoraria(8);
 		
 		Curso curso2 = new Curso();
 		curso2.setTitulo("Curso JavaScript");
 		curso2.setDescricao("Orientação a objeto no JS");
-		curso2.setCargaHoraria(2);
-		
+		curso2.setCargaHoraria(4);
+	
 		Mentoria mentoria = new Mentoria();
 		mentoria.setTitulo("Mentoria do Curso de Java");
 		mentoria.setDescricao("Mentoria sobre orientação a objeto");
 		mentoria.setData(LocalDate.now());
 		
 		
-		System.out.println(curso1);
+		/* System.out.println(curso1);
 		System.out.println(curso2);
-		System.out.println(mentoria);
+		System.out.println(mentoria); */
+		
+		Bootcamp bootcamp = new Bootcamp();
+		bootcamp.setNome("Bootcamp Java Developer");
+		bootcamp.setDescrição("Descrição do Boocamp Java Developer");
+		bootcamp.getConteudos().add(curso1);
+		bootcamp.getConteudos().add(curso2);
+		bootcamp.getConteudos().add(mentoria);
+		
+		Dev devVezio = new Dev();
+		devVezio.setNome("Vézio");
+		devVezio.inscreverBootcamp(bootcamp);
+		System.out.println("Conteúdos Inscritos Vézio" + devVezio.getConteudosInscritos());
+		devVezio.progredir();
+		System.out.println("-");
+		System.out.println("Conteúdos Inscritos Vézio" + devVezio.getConteudosInscritos());
+		System.out.println("Conteúdos Concluidos Vézio" + devVezio.getConteudosConcluidos());
+		devVezio.progredir();
+		System.out.println("-");
+		System.out.println("Conteúdos Inscritos Vézio" + devVezio.getConteudosInscritos());
+		System.out.println("Conteúdos Concluidos Vézio" + devVezio.getConteudosConcluidos());
+		System.out.println("XP: " + devVezio.calcularTotalXp());
+		
+		System.out.println("-------------");
+		
+		Dev devJoao = new Dev();
+		devJoao.setNome("João");
+		devJoao.inscreverBootcamp(bootcamp);
+		System.out.println("Conteúdos Inscritos João" + devJoao.getConteudosInscritos());
+		devJoao.progredir();
+		devJoao.progredir();
+		devJoao.progredir();
+		System.out.println("-");
+		System.out.println("Conteúdos Inscritos João" + devJoao.getConteudosInscritos());
+		System.out.println("Conteúdos Concluidos João" + devJoao.getConteudosConcluidos());
+		System.out.println("XP: " + devJoao.calcularTotalXp());
 	}
 
 }
